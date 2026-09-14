0873a06f8063dad8a6f59d220428299a5af0ac4d
Michal Haftarczyk <287037@student.pwr.edu.pl>
2026-09-15 00:49:12 +0200

    feat: report unknown commands and handle overflow

--- src/commands.c
+++ src/commands.c
@@ -32,15 +32,29 @@ void command_process(void){
     uint8_t c;
     static char commandBuffer[20];
     static uint8_t i = 0;
+    static uint8_t commandOverflow = 0;
 
     if(uart_read_byte(&c)){
+        if(commandOverflow == 0){
         if(c == '\n'){
             commandBuffer[i] = '\0';
             handle_command(commandBuffer);
             i = 0;
+    
         }else{
+            if(i<19){
             commandBuffer[i] = c;
             i++;
+            }
+            else{
+                commandOverflow = 1;
+            }
+        }
+    } else{
+        if(c == '\n'){
+            commandOverflow = 0;
+            i = 0;
         }
     }
+}
 }
